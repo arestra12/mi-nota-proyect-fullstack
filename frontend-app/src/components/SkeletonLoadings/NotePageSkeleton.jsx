5654cbbb24fdbc033d4255073bf6c3e0b1ad7dc6
Gabriel Claros <arestra12_@hotmail.com>
2026-04-27 21:23:47 -0600

    skeleton 2

--- frontend-app/src/components/SkeletonLoadings/NotePageSkeleton.jsx
+++ frontend-app/src/components/SkeletonLoadings/NotePageSkeleton.jsx
@@ -5,18 +5,13 @@ export const NotePageSkeleton = () => {
   return (
     <div className="note-skeleton">
 
-      {/* HEADER TITLE */}
       <div className="skeleton skeleton-title"></div>
 
-      {/* GRID */}
       <div className="note-skeleton-grid">
 
-        {/* LEFT IMAGE */}
         <div className="skeleton skeleton-image"></div>
 
-        {/* CENTER CONTENT */}
-        <div className="note-skeleton-center">
-
+        <div>
           <div className="skeleton skeleton-card"></div>
 
           <div className="skeleton-tags">
@@ -24,21 +19,15 @@ export const NotePageSkeleton = () => {
             <div className="skeleton skeleton-tag"></div>
             <div className="skeleton skeleton-tag"></div>
           </div>
-
         </div>
 
-        {/* RIGHT META */}
-        <div className="note-skeleton-right">
-
+        <div>
           <div className="skeleton skeleton-meta"></div>
-
           <div className="skeleton skeleton-meta small"></div>
-
         </div>
 
       </div>
 
-      {/* BUTTON */}
       <div className="skeleton skeleton-button"></div>
 
     </div>
